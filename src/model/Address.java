9a1f1f89381d3071c754b6907a8d7839fa71873d
luca <wissel.luca@gmail.com>
2015-05-27 18:19:32 +0200

    added a signUp form

---
+++ src/model/Address.java
@@ -0,0 +1,46 @@
+package model;
+
+/**
+ * Created by Luca on 27/05/2015.
+ */
+public class Address {
+
+
+    private String indirizzo;
+    private String citta;
+    private String nazione;
+    private String cap;
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    public String getNazione() {
+        return nazione;
+    }
+
+    public void setNazione(String nazione) {
+        this.nazione = nazione;
+    }
+
+    public String getCap() {
+        return cap;
+    }
+
+    public void setCap(String cap) {
+        this.cap = cap;
+    }
+
+}
---
+++ src/model/Carrello.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Created by Luca on 27/05/2015.
+ */
+public class Carrello {
+
+
+}
